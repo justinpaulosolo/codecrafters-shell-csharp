5479a484bff0d742cba981c42853e41d1ca1ecaf
Justin Solo <justin@justinsolo.com>
2026-07-12 01:55:55 -0700

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -12,6 +12,18 @@ class Program
 
             switch(splitResult[0])
             {
+                case "type":
+                    var type = string.Join(" ", splitResult[1..]);
+                    if (type == "echo" || type =="exit" || type == "type" )
+                    {
+                        Console.WriteLine($"{type} is a shell builtin", type);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{type} not found", type);
+                        break;
+                    }
                 case "echo":
                     var msg = string.Join(" ", splitResult[1..]);
                     Console.WriteLine(msg);
