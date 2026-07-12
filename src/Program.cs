e427dc4adaeb7e96ce783c8ced9b4a458faba3ad
Justin Solo <justin@justinsolo.com>
2026-07-12 01:25:32 -0700

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -2,7 +2,10 @@ class Program
 {
     static void Main()
     {
-        // TODO: Uncomment the code below to pass the first stage
         Console.Write("$ ");
+
+        var command = Console.ReadLine();
+
+        Console.WriteLine($"{command}: command not found");
     }
 }
